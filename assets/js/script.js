a8cdb8a7b21cb45a0ccc37c1ca80f04dfaa6d89d
Nafis Rahman <nmr278@nyu.edu>
2021-07-28 15:35:38 -0400

    News card displays image, news title, and news text

--- assets/js/script.js
+++ assets/js/script.js
@@ -84,31 +84,56 @@ DISPLAY FINANCIAL NEWS - NAFIS
 DEPENDENCIES
 ==============*/
 
+var na_newsCards = $('.news-container');
+
 /*==============
 DATA
 ==============*/
 
-var stockTicker = 'AAPL';
-var APIKey = '4e48677e67d7cfd40210605712bdb9a0';
-var newsUrl = `https://financialmodelingprep.com/api/v3/stock_news?tickers=${stockTicker}&limit=5&apikey=${APIKey}`;
+var na_stockTicker = 'AAPL';
+var na_APIKey = '4e48677e67d7cfd40210605712bdb9a0';
+var na_newsUrl = `https://financialmodelingprep.com/api/v3/stock_news?tickers=${na_stockTicker}&limit=5&apikey=${na_APIKey}`;
+var na_stock_news_array = [];
 
 /*==============
 FUNCTIONS
 ==============*/
 
-function createNewsCard () {
-  var card = $()
+function createNewsCard (i) {
+  var na_card = $('<div>');
+  var na_cardNewsTitle = $('<h2>');
+  var na_cardNewsText = $('<p>');
+  var na_newsImageUrl = na_stock_news_array[i].image;
+  var na_stockNewsTitle = na_stock_news_array[i].title;
+  var na_stockNewsText = na_stock_news_array[i].text;
+  
+  na_card.attr('class','news-card')
+  
+  na_card.css({"height": "200px", "background-image": `url(${na_newsImageUrl})`, "font-size": "200%"});
+  na_cardNewsTitle.text(na_stockNewsTitle);
+  na_cardNewsText.text(na_stockNewsText);
+  console.dir(na_card)
+
+  na_newsCards.append(na_card);
+  na_card.append(na_cardNewsTitle);
+  na_card.append(na_cardNewsText);
 }
 
 /*==============
 INITIALIZATION
 ==============*/
 
-fetch(newsUrl)
+fetch(na_newsUrl)
   .then(function (response) {
   return response.json();
 })
   .then(function (data) {
   console.log('Fetch Response \n-------------');
   console.log(data);
+  na_stock_news_array.push(data[0])
+  createNewsCard(0)
 });
+
+na_newsCards.on('click','.news-card', function () {
+  console.log('open new tab');
+})
