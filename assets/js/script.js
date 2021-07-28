66acdf761637c8b13e3f07d9f85ade590a5ad50a
Nafis Rahman <nmr278@nyu.edu>
2021-07-28 19:23:31 -0400

    Stock news cards correctly render

--- assets/js/script.js
+++ assets/js/script.js
@@ -90,29 +90,59 @@ var na_newsCards = $('.news-container');
 DATA
 ==============*/
 
-var na_stockTicker = 'AAPL';
+var na_favStocks = ['AAPL','GOOG','GME','WDC'];
+// var na_stockTicker = 'AAPL'; 
 var na_APIKey = '4e48677e67d7cfd40210605712bdb9a0';
-var na_newsUrl = `https://financialmodelingprep.com/api/v3/stock_news?tickers=${na_stockTicker}&limit=5&apikey=${na_APIKey}`;
-var na_stock_news_array = [];
-
+var na_newsUrl = '' // `https://financialmodelingprep.com/api/v3/stock_news?tickers=${na_stockTicker}&limit=5&apikey=${na_APIKey}`;
+var na_stocksNewsArray = [];
+var na_favStocksString = '';
+var na_article_limit = na_favStocks.length*3;
 /*==============
 FUNCTIONS
 ==============*/
 
-function createNewsCard (i) {
+function stringifyFavStocks (list) {
+  var stringifiedList = '';
+  for (var i=0; i<list.length-1; i++) {
+    // Appends each stock ticker to a comma-separated string for API call
+    stringifiedList += list[i] + ',';
+  }
+  stringifiedList += list[length-1];
+  return stringifiedList;
+};
+
+function renderFaveStockCards () {
+  
+ // fetch 
+
+  for (var i=0; i<na_favStocks.length; i++) {
+    var na_stockTicker = na_favStocks[i];
+    
+    for (var j=0; j<na_stocksNewsArray.length; j++) {
+      var na_stockNews = na_stocksNewsArray[j];
+      
+      if (na_stockNews.symbol === na_stockTicker) {
+        console.log(na_stockNews);
+        renderNewsCard(na_stockNews);
+      };
+    };
+  };
+};
+
+function renderNewsCard (stockData) {
   var na_card = $('<div>');
   var na_cardNewsTitle = $('<h2>');
   var na_cardNewsText = $('<p>');
-  var na_newsImageUrl = na_stock_news_array[i].image;
-  var na_stockNewsTitle = na_stock_news_array[i].title;
-  var na_stockNewsText = na_stock_news_array[i].text;
+  var na_newsImageUrl = stockData.image;
+  var na_stockNewsTitle = stockData.title;
+  var na_stockNewsText = stockData.text;
   
   na_card.attr('class','news-card')
   
   na_card.css({"height": "200px", "background-image": `url(${na_newsImageUrl})`, "font-size": "200%"});
   na_cardNewsTitle.text(na_stockNewsTitle);
   na_cardNewsText.text(na_stockNewsText);
-  console.dir(na_card)
+  console.dir(na_card);
 
   na_newsCards.append(na_card);
   na_card.append(na_cardNewsTitle);
@@ -123,6 +153,9 @@ function createNewsCard (i) {
 INITIALIZATION
 ==============*/
 
+na_favStocksString = stringifyFavStocks(na_favStocks);
+na_newsUrl = `https://financialmodelingprep.com/api/v3/stock_news?tickers=${na_favStocksString}&limit=${na_article_limit}&apikey=${na_APIKey}`;
+
 fetch(na_newsUrl)
   .then(function (response) {
   return response.json();
@@ -130,8 +163,9 @@ fetch(na_newsUrl)
   .then(function (data) {
   console.log('Fetch Response \n-------------');
   console.log(data);
-  na_stock_news_array.push(data[0])
-  createNewsCard(0)
+  na_stocksNewsArray = data; // do you index data?
+  console.log(na_stocksNewsArray);
+  renderFaveStockCards();
 });
 
 na_newsCards.on('click','.news-card', function () {
